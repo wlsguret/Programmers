3997711fb380484d9b0a9d13f5a5dcfa104a73c2
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-01-19 01:30:24 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.14 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int width = wallpaper[0].length();
+        int height = wallpaper.length;
+        
+        int[] answer = {height,width,0,0}; 
+        
+        int idx = 0;
+        for(String row : wallpaper) {
+            int index = row.indexOf("#");
+            
+            
+            if(index != -1) {
+                if(index < answer[1]) {
+                    // 시작 x 좌표
+                    answer[1] = index;
+                }
+            }
+            
+            int lastIndex = row.lastIndexOf("#");
+            if(lastIndex != -1) {
+                if(lastIndex >= answer[3]) {
+                    // 끝 x 좌표
+                    answer[3] = ++lastIndex;
+                }
+                
+                if(idx < answer[0]) {
+                    // 시작 y 좌표
+                    answer[0] = idx;
+                }
+                
+                // 끝 y 좌표
+                answer[2] = idx + 1;
+            }
+            
+            idx++;
+        }
+        
+        return answer;
+    }
+}
