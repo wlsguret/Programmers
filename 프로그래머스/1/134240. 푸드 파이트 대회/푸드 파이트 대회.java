ab863f10dcfb4b81c9e0505bf99448367139ca6c
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-01-27 16:20:29 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.03 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1; i<food.length; i++) {
+            int num = food[i];
+            if(num > 1) {
+                for(int j =0; j< num/2;j++) {
+                    sb.append(i);
+                }
+            }
+        }
+        
+        return sb.toString() + "0" + sb.reverse().toString();
+    }
+}
