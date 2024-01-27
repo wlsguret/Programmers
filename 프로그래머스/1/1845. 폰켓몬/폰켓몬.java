572fcd155a8077a6f54c456bd5f8301260430739
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-01-27 17:48:20 +0900

    [level 1] Title: 폰켓몬, Time: 3.79 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set set = new HashSet();
+    
+        for(int num : nums) {
+            set.add(num);
+        }
+        
+        return set.size() > nums.length/2 ? nums.length/2 : set.size();
+    }
+}
