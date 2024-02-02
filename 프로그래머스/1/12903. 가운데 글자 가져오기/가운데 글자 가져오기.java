11a24f594c31e15692959a562e72f651563ce83f
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-02-02 23:32:46 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int half = s.length() / 2;
+        if(s.length() % 2 == 0) {
+            answer = s.substring(half -1, half +1);
+        } else {
+            answer = s.substring(half, half +1);
+        }
+        
+        return answer;
+    }
+}
