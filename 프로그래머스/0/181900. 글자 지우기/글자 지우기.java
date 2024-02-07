53715fc5b044591b546e67f484ac7916cf5c7dd4
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-02-08 00:31:21 +0900

    [level 0] Title: 글자 지우기, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,12 +1,12 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
         // 성능 최적화
-        char[] chars = my_string.toCharArray();
-        for(int i = indices.length -1; i >= 0; i--) {
-            chars[indices[i]] = '\0';
+        char[] arr = my_string.toCharArray();
+        for(int i : indices) {
+            arr[i] = '\0';
         }
         StringBuilder sb = new StringBuilder();
-        for(char c : chars) {
+        for(char c : arr) {
             if(c != '\0') sb.append(c);
         }
         
