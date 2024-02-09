7bbd56cfa3c4e0d8df566a82a94993ca4e02cd83
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-02-09 23:09:55 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.54 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,9 +3,9 @@ class Solution {
         String[] strArr = s.split(" ");
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        
+        int tmp;
         for(String num : strArr) {
-            int tmp = Integer.parseInt(num);
+            tmp = Integer.parseInt(num);
             if(max < tmp) max = tmp;
             if(tmp < min) min = tmp;
         }
