b9d0765688bc0232f3d8360e29e44e3af00e40f7
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-02-11 16:51:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for(int i = 1; i < 10; i++) {
+            answer += i;
+        }
+        
+        for(int num : numbers) {
+            answer -= num;
+        }
+        return answer;
+    }
+}
