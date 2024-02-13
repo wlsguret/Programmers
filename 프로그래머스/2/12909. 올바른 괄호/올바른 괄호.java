814b3aa430cabc5e63ecd1e4a4b12f52543d2046
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-02-13 23:45:01 +0900

    [level 2] Title: 올바른 괄호, Time: 6.54 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,22 +1,26 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
-        int count = 0;
-        for(int i = 0; i<s.length();i++){
-            if(s.charAt(i) == '('){
-                count++;
+        // 길이가 홀수 일때 실패
+        if(s.length() % 2 != 0) return false;
+        // 닫힘으로 시작 실패
+        else if(s.startsWith(")")) return false;
+        // 열림으로 종료 실패
+        else if(s.endsWith("(")) return false;
+
+        int cnt = 0;
+        for(char c : s.toCharArray()) {
+            switch(c) {
+                case '(' :
+                    cnt++;
+                    break;
+                case ')' :
+                    cnt--;
+                    break;
             }
-            if(s.charAt(i) == ')'){
-                count--;
-            }
-            if(count < 0){
-                break;
-            }
-        }
-        if(count == 0){
-            answer = true;
+            if(cnt < 0) return false;
         }
 
-        return answer;
+        return cnt == 0;
     }
+
 }
