d5012ac475807a3869812a11d5304227a47fc491
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-02-29 22:47:26 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.09 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,16 +1,9 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         StringBuilder answer = new StringBuilder();
-        for(int i = 0; i < my_string.length(); i++) {
-            if(i < s) {
-                answer.append(my_string.charAt(i));    
-            } else if(i == s) {
-                answer.append(overwrite_string);
-                i += overwrite_string.length() -1;
-            } else {
-                answer.append(my_string.charAt(i));    
-            }
-        }
+        answer.append(my_string.substring(0, s));
+        answer.append(overwrite_string);
+        answer.append(my_string.substring(s + overwrite_string.length()));
         return answer.toString();
     }
 }
