f09852f93da59b8d5986dd85463031f6527e66a1
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-01 23:33:28 +0900

    [level 1] Title: K번째수, Time: 0.43 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int i = 0;
+        for (int[] command : commands) {
+            int from = command[0] -1;
+            int to = command[1];
+            int idx = command[2] -1;
+            int[] arr = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(arr);
+            answer[i++] = arr[idx];
+        }    
+        return answer;
+    }
+}
