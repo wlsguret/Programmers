f6e9a5db7c3f7d5ea25ee420498bf4b6f6d022d0
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-03 23:47:42 +0900

    [level 3] Title: 네트워크, Time: 2.73 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        // 네트워크 키 초기화
+        // 각자 index 를 네트워크 ID로 가지고 있음
+        int[] networks = new int[n];
+        for(int i = 0; i < n; i++) {
+            networks[i] = i;
+        }    
+        
+        for(int i = 0; i < n; i++) {
+            int[] networkInfo = computers[i];
+            int ip = networks[i];
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                
+                int isConnected = networkInfo[j];
+                if(isConnected == 1) {
+                    int oldIp = networks[j];
+                    networks[j] = ip;
+                    // 연결된 네트워크 전부 동기화
+                    for(int x = 0; x < n; x++) {
+                        if(networks[x] == oldIp) {
+                            networks[x] = ip;
+                        }
+                    }
+                }
+            }
+        }
+        
+        HashSet<Integer> result = Arrays.stream(networks)
+          .boxed()
+          .collect(Collectors.toCollection(HashSet::new));  
+
+        return result.size();
+    }
+}
