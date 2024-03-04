cadc74d66c2b539dff8befbcb17a1f784e485f4d
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-05 00:59:10 +0900

    [level 3] Title: 야근 지수, Time: 129.08 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return - Integer.compare(o1,o2);
+            }
+        });
+        for(int work : works) {
+            maxHeap.add(work);
+        }
+        
+        for(int i = n; i > 0; i--) {
+            Integer a = maxHeap.poll();
+            if(a == null) break;
+            
+            a--;
+            
+            if(a > 0) maxHeap.add(a);
+        }
+        
+        Iterator<Integer> i = maxHeap.iterator();
+        while(i.hasNext()) {
+            int a = i.next();
+            answer += a * a;
+        }
+       
+        return answer;
+    }
+    
+  
+}
