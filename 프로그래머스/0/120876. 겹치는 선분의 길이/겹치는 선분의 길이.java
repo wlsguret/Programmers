fa9a83e63d3d749cf2c44eb2b7036a06ed3320b1
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-11 22:51:28 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int solution(int[][] lines) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int[] line : lines) {
+            for(int start = line[0]; start < line[1]; start++) {
+                map.put(start, map.getOrDefault(start,0) + 1);
+            }
+        }
+        int answer = 0;
+        for(int i : map.values()) {
+            if(i > 1) answer++;
+        }
+        return answer;
+    }
+}
