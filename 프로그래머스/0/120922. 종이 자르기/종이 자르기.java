df7aec4d6aa284e442e5e5f83d5b755527f5e7f4
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-13 00:16:00 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-        int answer = 0;
-        answer += M - 1;
-        answer += (N - 1) * M;
-        return answer;
+        return M * N -1;
     }
 }
