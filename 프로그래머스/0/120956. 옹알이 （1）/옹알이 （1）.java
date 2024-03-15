5dcc7a98c5fa2a56825c040d261e85464a5c9165
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-15 22:25:01 +0900

    [level 0] Title: 옹알이 (1), Time: 0.20 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String word : babbling) {
+            if(check(word)) answer++;
+        }    
+        return answer;
+    }
+    
+    boolean check(String word) {
+        return word.replace("aya", " ")
+            .replace("ye", " ")
+            .replace("woo", " ")
+            .replace("ma", " ")
+            .isBlank();
+    }
+}
