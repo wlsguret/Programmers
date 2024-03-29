225c3dd816b6b4a2f1c45ca46086202b8526ccfd
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-29 23:50:04 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("*".repeat(Math.max(0, phone_number.length() - 4)));
+
+        sb.append(phone_number.substring(phone_number.length() -4));
+
+        return sb.toString(); 
+    }
+}
