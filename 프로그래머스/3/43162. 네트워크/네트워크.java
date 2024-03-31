a590d09faeda789b98ae2f9ecc708ca49599c7d4
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-03-31 22:17:09 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -14,7 +14,7 @@ class Solution {
         for(int i = 0; i < n; i++) {
             int[] networkInfo = computers[i];
             int ip = networks[i];
-            for(int j = 0; j < n; j++) {
+            for(int j = 0; j < n; j++) { 
                 if(i == j) continue;
                 
                 int isConnected = networkInfo[j];
