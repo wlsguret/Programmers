ac8c929fbba0731ef9c61ed5d570be3daccbba7f
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-04-25 23:42:44 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.45 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -11,7 +11,7 @@ class Solution {
             }
             if(count > limit) {
                 answer += power;
-            } else {
+            } else { 
                 answer += count;
             }
             
