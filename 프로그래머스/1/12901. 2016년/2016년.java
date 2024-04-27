b9dc4078dd0cd9d3ccc608b996629f62ff903af2
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-04-27 23:09:29 +0900

    [level 1] Title: 2016년, Time: 0.97 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -4,7 +4,7 @@ import java.time.DayOfWeek;
 class Solution {
     public String solution(int a, int b) {
         LocalDate date = LocalDate.of(2016, a, b);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        DayOfWeek dayOfWeek = date.getDayOfWeek(); 
         return dayOfWeek.toString().substring(0,3);
     }
 }
