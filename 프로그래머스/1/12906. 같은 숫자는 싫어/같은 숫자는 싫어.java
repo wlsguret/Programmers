9f301a7b53ab0e26e19cd4a74435511a29ec8d9b
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-05-02 22:48:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 15.11 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,16 +1,25 @@
-import java.util.*;
-
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = new int[arr.length];
-        
-        int cnt = 0; 
+        int cnt = 0;
         int tmp = -1;
+        for(int i = 0; i < arr.length; i++) {
+            if (tmp == arr[i]) {
+                arr[i] = -1;
+                cnt++;
+            } else {
+                tmp = arr[i];
+            }
+        }
+        
+        int[] answer = new int[arr.length - cnt];
+        
+        int j = 0;
         for(int num : arr) {
-            if(tmp != num) answer[cnt++] = num;
-            tmp = num;
-        } 
+            if(num != -1) {
+                answer[j++] = num;
+            }
+        }
 
-        return Arrays.copyOf(answer, cnt);
+        return answer;
     }
 }
