723b78e8e5f2bf34d4c0bc169e612cc09af006fc
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-06-19 23:05:37 +0900

    [level 2] Title: 피보나치 수, Time: 3.30 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        long[] arr = new long[n];
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3; i < n; i++) {
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+        }
+        return (int) arr[n-1];
+    }
+}
