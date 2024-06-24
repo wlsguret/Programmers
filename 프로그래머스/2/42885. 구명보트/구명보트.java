9b3d96559f4d9bd891d07aae235b9fb4476eff27
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-06-25 00:58:42 +0900

    [level 2] Title: 구명보트, Time: 7.47 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,27 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
         Arrays.sort(people);
-
-        int min = 0;
-        int max = people.length - 1;
-        
-        for(int i = max; i >= 0; i--) {
-            if(i == min) {
-                answer++;
-                break;
-            } else if(i < min) {
-                break;
-            }
-            if(people[i] + people[min] <= limit) {
-                answer++;
-                min++;
-            } else {
-                answer++;
-            }
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
         }
-       
-        return answer;
+        return people.length - i;
     }
 }
