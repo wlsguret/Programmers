8098c29d371a9220edc9651ce051117c7e648e77
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-07-10 00:04:33 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 0.28 ms
+메모리: 96.9 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 10일 00:03:51
+2024년 07월 10일 00:04:41
 
 ### 문제 설명
 
--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -1,20 +1,7 @@
 class Solution {
     public long solution(int a, int b) {
         if(a == b) return a;
-        
-        int tmp = a;
-        if(b > a) {
-            a = b;
-            b = tmp;
-        }
-        
-        long cnt = (long) Math.ceil((a - b) / 2);
-        long answer = (a + b) * cnt;
-        
-        if ((a - b) % 2 == 0) {
-            answer += (a + b) / 2;
-        }
-        return (long) (b+a)*(Math.abs(b-a)+1)/2;
-        //return answer;
+
+        return (long) (b+a) * (Math.abs(b-a)+1) / 2;
     }
 }
