4fe04668a3cd5e6dc50a5fe63c4acff9a0cf94b2
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-08-06 23:35:44 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length + 1];
+        int[] answer = new int[num_list.length + 1]; 
         
         for(int i = 0; i < num_list.length; i++) {
             answer[i] = num_list[i];
