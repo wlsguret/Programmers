ca169facd4b5fde9f1cc913ba35b09081b2c339f
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-08-14 22:59:37 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 23.22 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String myString) {
         String answer = "";
         for(char c : myString.toCharArray()) {
-            if(c < 90) {
+            if(c <= 'Z') {
                 c += 32;
             }
             answer += c;
