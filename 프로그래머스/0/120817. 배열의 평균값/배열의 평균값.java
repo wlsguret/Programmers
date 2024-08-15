b6dae89bde3b24b758324e8ea6186a821117d50e
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-08-15 23:45:05 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        
+        for (int number : numbers) {
+            answer += number; 
+        }
+        answer /= numbers.length;
+        return answer;
+    }
+}
