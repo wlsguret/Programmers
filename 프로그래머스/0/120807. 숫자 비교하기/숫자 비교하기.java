92ec73717124a5b1d3c00a0e3f62b42bb306ac8d
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-08-17 23:20:48 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
         
-        if (num1 == num2) return 1;
+        if (num1 == num2) return 1; 
             else return -1;
     } 
 }
