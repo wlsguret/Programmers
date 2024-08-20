0357465eb105a014328b061fa477e4afbb34286a
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-08-20 23:50:37 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int hp) {
         int answer = 0;
         answer += hp / 5;
-        hp = hp % 5;
+        hp %= 5;
         
         answer += hp / 3;
-        hp = hp % 3;
+        hp %= 3;
         
-        answer += hp;
+        answer += hp / 1;
         
         return answer;
     }
