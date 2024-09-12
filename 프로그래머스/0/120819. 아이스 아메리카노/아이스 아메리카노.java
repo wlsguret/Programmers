546a845d5abab465aafa649178ec528d2fb35307
Doritos <83631924+wlsguret@users.noreply.github.com>
2024-09-12 22:59:11 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int money) {
         final int price = 5500;
-        return new int[] {money / price, money % price};
+        return new int[] {money / price, money % price}; 
     }
 }
